d3985f474565ab38b83b3485d4156bbb862e381c
RichieBernal <A01688410@tec.mx>
2023-08-07 18:39:33 -0700

    s

--- venv9/cookie_test/itesm_mlops/load/load_data.py
+++ venv9/cookie_test/itesm_mlops/load/load_data.py
@@ -22,50 +22,13 @@ class DataRetriever:
     ```
     """
 
-    DROP_COLS = ['name', 'ticket', 'boat', 'body', 'home.dest']
-    DATASETS_DIR = './data/'  # Directory where data will be saved.
+#    DROP_COLS = ['name', 'ticket', 'boat', 'body', 'home.dest']
+#    DATASETS_DIR = './data/'  # Directory where data will be saved.
     RETRIEVED_DATA = 'retrieved_data.csv'  # File name for the retrieved data.
 
     def __init__(self, url):
         self.url = url
-
-    def _get_first_cabin(self, row):
-        """
-        Helper function to extract the first cabin from a row.
-
-        Parameters:
-            row (str): The row containing cabin information.
-
-        Returns:
-            str: The first cabin from the row, or np.nan if not found.
-        """
-        try:
-            return row.split()[0]
-        except Exception:
-            return np.nan
-
-    def _get_title(self, passenger):
-        """
-        Helper function to extract the title from a passenger's name.
-
-        Parameters:
-            passenger (str): The name of the passenger.
-
-        Returns:
-            str: The title extracted from the passenger's name.
-        """
-        line = passenger
-        if re.search('Mrs', line):
-            return 'Mrs'
-        elif re.search('Mr', line):
-            return 'Mr'
-        elif re.search('Miss', line):
-            return 'Miss'
-        elif re.search('Master', line):
-            return 'Master'
-        else:
-            return 'Other'
-
+    
     def retrieve_data(self):
         """
         Retrieves data from the specified URL, processes it, and stores it in a CSV file.
@@ -75,15 +38,6 @@ class DataRetriever:
         """
         # Loading data from specific URL
         data = pd.read_csv(self.url)
-
-        # Uncovering missing data
-        data.replace('?', np.nan, inplace=True)
-        data['age'] = data['age'].astype('float')
-        data['fare'] = data['fare'].astype('float')
-
-        # Extract the first cabin | Extract the title from 'name'
-        data['cabin'] = data['cabin'].apply(self._get_first_cabin)
-        data['title'] = data['name'].apply(self._get_title)
 
         # Drop irrelevant columns
         data.drop(self.DROP_COLS, axis=1, inplace=True)
